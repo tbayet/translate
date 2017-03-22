941d81f29ff2cb93914fa4c1d768ca93dbcedddb
U0_a220 <bayet.thomas@gmail.com>
2017-03-22 18:44:08 +0100

    to Angular

---
+++ srt_to_json.php
@@ -0,0 +1,26 @@
+<?php
+	$srt_ar = array();
+	$file = fopen("./files/test.srt", "r");
+	$nb;
+	$buff;
+	$i;
+
+	while ($nb = fgets($file))
+	{
+		$nb = substr($nb, 0, strlen($nb) - 2);
+		if (is_numeric($nb))
+		{
+			$nb = intval($nb) - 1;
+			$i = 0;
+			$srt_ar[$nb] = array();
+			$buff = fgets($file);
+			$srt_ar[$nb][$i++] = substr($buff, 0, strlen($buff) - 2);
+			while(($buff = fgets($file)) && strcmp($buff, "\r\n") !== 0)
+				$srt_ar[$nb][$i++] = utf8_encode(substr($buff, 0, strlen($buff) - 2));
+		}
+	}
+	$JSONfile = fopen("db/project.json", "w+");
+	fwrite($JSONfile, json_encode($srt_ar));
+	fclose($file);
+	fclose($JSONfile);
+?>
